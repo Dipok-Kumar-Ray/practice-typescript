a31ffe7b9d556f7bf6db34df56cc2a50ca97095a
Dipok-Kumar-Ray <dipok7316@gmail.com>
2025-11-23 02:40:56 +0600

    tuple.ts

---
+++ user-defined-type/tuple.js
@@ -0,0 +1,6 @@
+//mixed data - key, value
+var user;
+user = [101, "dipok"];
+user.push(102, "Disha");
+console.log(user);
+console.log(user[0]);
---
+++ user-defined-type/tuple.ts
@@ -0,0 +1,9 @@
+//mixed data - key, value
+
+let user: [number, string]
+ user = [101, "dipok"];
+
+user.push(102, "Disha");
+
+ console.log(user);
+ console.log(user[0]);
