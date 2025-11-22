c03fc3e605acb4d2d62935ce473fd627e4ae9d39
Dipok-Kumar-Ray <dipok7316@gmail.com>
2025-11-23 02:22:12 +0600

    array types

---
+++ user-defined-type/array-type.js
@@ -0,0 +1,7 @@
+var userName;
+userName = ["Dipok", "shakhawat", "Mehedi"];
+console.log(userName[2]);
+userName.sort();
+console.log(userName);
+var multipleTypes;
+multipleTypes = [1, "name"];
---
+++ user-defined-type/array-type.ts
@@ -0,0 +1,14 @@
+let userName: string[];
+userName = ["Dipok", "shakhawat", "Mehedi"];
+
+console.log(userName[2]);
+
+userName.sort();
+console.log(userName);
+
+
+
+let multipleTypes : (string | number)[];
+multipleTypes = [1, "name"];
+
+
